f14468606f075c9027955e20d98fe2be525e8445
yibarr <yael@os.city>
2020-06-06 15:39:35 -0500

    Starter endpoint template

--- api/controllers/session.js
+++ api/controllers/session.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-router.use( '/',(req,res) => {
-  res.send('Hola')
+router.use( '/solo/solito',(req,res) => {
+  res.send('Hola mundo')
+})
+
+router.post('/:id',async (req, res) => {
+  try {
+    const params = req.params
+    
+    if (!params.id) {
+      throw new Error('Oye veniste sin id')
+    }
+
+   // const session = getSessionById(params.id)
+
+
+    res.send(session)
+  } catch (error) {
+    res.send({error: error.message})
+  }
 })
 
 module.exports = router
